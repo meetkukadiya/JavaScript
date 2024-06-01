d7355ca1ef76733abcc4bd69becba504b53530fd
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-01 06:11:54 +0000

    datatypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Are you sure?")  // Don't use we use in NodeJS 
+
+console.log("How's are going on?");
+
+console.log(4 + 3);
+
+console.log(typeof "Meet");
+console.log(typeof 345);
+
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
