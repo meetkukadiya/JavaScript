7b310dc2487636fb7a762878b647fedecb8fffce
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-01 07:30:45 +0000

    Conversion of Datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "75"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false 
+// "Meet" => true, "" => false
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
