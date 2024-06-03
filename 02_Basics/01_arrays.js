e2a9ada9aef17520c63b4a1a008832052b5a4ea8
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-03 12:07:41 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,20 @@
+// array 
+
+const myArr =  [12, 23, 45, 64, 86]
+
+console.log(myArr[3]);
+
+myArr.push(34)
+console.log(myArr)
+console.log(myArr.includes(64))
+
+// myArr.unshift(68)
+// myArr.shift()
+// console.log(myArr);
+
+const newArr = myArr.join()
+console.log(newArr);
+
+// slice and splice
+
+
