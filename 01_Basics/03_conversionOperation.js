f94973ee30a18435decad28acd558382193c508d
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-03 05:06:54 +0000

    comparison of datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -20,3 +20,26 @@ let someNumber = 35
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+//  Operations 
+
+let value = 54
+let negVal = -value
+
+console.log(negVal);
+
+let str1 = "Hello, "
+let str2 = "Meet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(true);
+console.log(+true);
+console.log(+false);
+console.log(+"");
+
+let gameCounter = 244
+gameCounter++
+
+console.log(gameCounter);
