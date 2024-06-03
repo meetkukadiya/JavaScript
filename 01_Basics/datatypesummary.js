c05ec850c3b3e17fcf7ab578d9da42dd9f729ba7
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-03 05:50:43 +0000

    datatypes summary

---
+++ 01_Basics/datatypesummary.js
@@ -0,0 +1,44 @@
+// Primitive 
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail; 
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 234235362342342342n
+
+
+// Reference (Non-primitive)
+
+// Arrays, Objects, Functions
+
+// array
+const cars = ["BMW A21", "Audi A4", "Volvo V90"];
+
+// object
+let studentDetails = {
+    name: "Meet",
+    age: 21,
+}
+
+// function 
+
+const myFun = function(){
+    console.log("Hello World!");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof myFun);
+console.log(typeof studentDetails);
+console.log(typeof cars);
