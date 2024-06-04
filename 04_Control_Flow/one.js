5f40beea0bf89bc0aeecb8234f5df9eb10720324
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-04 13:49:00 +0000

    control flow (if else)

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,34 @@
+// if
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+
+}
+
+if (3 !== "3"){  // Execute the statement because it is true. (Check the datatype)
+    console.log("Execute the Code 1.");
+}
+
+if (3 != "3"){   // Execute the statement because condition is false (Not Check datatype)
+    console.log("Execute the Code 2.");
+}
+
+const temperature = 42
+
+if(temperature > 41){
+    console.log("Heat wave alert ⚠️, Stay safe at Home.🏠");
+}
+else{
+    console.log("It is a normal temperature");
+}
+
+const speed = 234
+
+if(speed > 150){
+    let power = 'Fly'
+    console.log(`Your Plane can ${power}.`);
+}
+
+// console.log(`Your Plane can ${power}.`);
+
