cce14c815febfb016f942e27828421ea3cbe8214
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-05 10:11:25 +0000

    foreach loop for object converted into arrat for data

---
+++ 05_Iteration/five.js
@@ -0,0 +1,34 @@
+const coding = ["js", "ruby", "python", "cpp"]
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// } )
+
+// coding.forEach( (item)=> {  
+//     console.log(item);
+// })
+     
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)  
+
+
+const myCoding = [
+    {
+    languageName: "JavaScript",
+    languageFileName: "js"
+},
+{
+    languageName: "Python",
+    languageFileName: "py"
+},
+{
+    languageName: "Java",
+    languageFileName: "java"
+}]
+
+myCoding.forEach((item) => {
+    console.log(item); 
+})
