4bfad29ebcd35161223d39c2f527e1804723ed03
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-06 09:18:37 +0000

    pro 1 & 2 assessment

---
+++ assessment/program1.js
@@ -0,0 +1,12 @@
+// 1 . Check if a number is positive, negative, or zero using if-else.
+
+
+const num = -34;
+
+if(num > 0){
+    console.log("The number is Positive.");
+} else if (num < 0 ) {
+    console.log("The number is Negative.");
+} else {
+    console.log("The given number is Zero.")
+}
