6ccbe94cc21f90150924d61913de1493107d5fa6
Meet Kukadiya <112765061+meetkukadiya@users.noreply.github.com>
2024-06-07 05:32:48 +0000

    pattern8 changes

--- assessment/Patterns/pattern8.js
+++ assessment/Patterns/pattern8.js
@@ -2,7 +2,7 @@
 
 let num = 4;
 
-for (let i = 0; i <= num; i++) {
+for (let i = 1; i <= num; i++) {
     
     let newL = "";
 
@@ -16,15 +16,15 @@ for (let i = 0; i <= num; i++) {
     console.log(newL);
 }
 
-for (let i = 0; i <= num ; i++) {
+for (let i = 1; i <= num ; i++) {
     
     let newL1 = "";
 
     for (let k = 0; k < i ; k++) {
         newL1 += " ";
     }
-    for (let j = 0; j < num - i + 1 ; j++) {
-        newL1 += "* ";
+    for (let j = 0; j < num - i  ; j++) {
+        newL1 += " *";
     }
 
     console.log(newL1);
